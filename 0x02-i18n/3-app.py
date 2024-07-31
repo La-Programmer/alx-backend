d29186afbb1c806d99779627d1a3984eceba2009
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-07-31 13:17:54 +0100

    Fix task 3 again

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
-"""Flask application"""
+"""Flask application Module"""
 
 from flask import Flask, render_template, request
 from flask_babel import Babel, gettext
 
 
 class Config:
-    """Config Class"""
+    """Config Class for flask app"""
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = LANGUAGES[0]
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 def get_locale() -> str:
-    """Get locale"""
+    """Get locale for flask app"""
     return request.accept_languages.best_match(Config.LANGUAGES)
 
 
@@ -25,12 +25,12 @@ app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
 
 @app.route("/")
 def hello() -> str:
-    """Root route"""
+    """Root route for flask app"""
     return render_template('3-index.html')
 
 
 if __name__ == '__main__':
-    """Main Function"""
+    """Main Function for flask app"""
     host = '0.0.0.0'
     port = '5000'
     app.run(host=host, port=port, threaded=True)
