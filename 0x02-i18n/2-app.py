a0b73390c9b9dce63c1cfe3864e5af5bb7c550ba
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-07-31 13:09:36 +0100

    Fix task 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,36 +1,37 @@
 #!/usr/bin/env python3
-"""Flask application"""
+"""Flask application Module"""
 
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
 class Config:
-    """Config Class"""
+    """Config Class for flask app using babel"""
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = LANGUAGES[0]
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-def get_locale() -> str:
-    """Get locale"""
-    return request.accept_languages.best_match(Config.LANGUAGES)
-
-
 app = Flask(__name__)
-babel = Babel(app, locale_selector=get_locale)
+babel = Babel(app)
 app.config['BABEL_DEFAULT_LOCALE'] = Config.LANGUAGES[0]
 app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
 
 
+@babel.localeselector
+def get_locale() -> str:
+    """Get locale function"""
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
 @app.route("/")
 def hello() -> str:
-    """Root route"""
+    """Root route for flask app"""
     return render_template('2-index.html')
 
 
 if __name__ == '__main__':
-    """Main Function"""
+    """Main Function for flask app"""
     host = '0.0.0.0'
     port = '5000'
     app.run(host=host, port=port, threaded=True)
