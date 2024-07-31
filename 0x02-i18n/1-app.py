48bc6bcc2a0d0eb7d13950f4dbfe61ea4dc4f818
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-07-31 13:08:26 +0100

    Fix task 1 again

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -24,7 +24,7 @@ def hello():
 
 
 if __name__ == '__main__':
-    """Main Function"""
+    """Main Function for flask app"""
     host = '0.0.0.0'
     port = '5000'
     app.run(host=host, port=port, threaded=True)
